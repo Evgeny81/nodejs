59d3f66835ba5c36749153916b07d42170f45726
Evgeny_Kuznetsov <evgeny_kuznetsov@epam.com>
2019-03-17 09:06:40 +0300

    Homework 7 (task 1 - 4)

--- http-servers/json-server.js
+++ http-servers/json-server.js
@@ -1,19 +1,36 @@
-const product = {
-    id: 1,
-    name: 'Supreme T-Shirt',
-    brand: 'Supreme', price: 99.99,
-    options: [{color: 'blue'}, {size: 'XL'}]
-};
+const MongoClient = require('mongodb').MongoClient;
 
 require('http')
     .createServer()
     .on('request', (req, res) => {
         const {url} = req;
+        const mongoClient = new MongoClient("mongodb://localhost:27017/", {useNewUrlParser: true});
         res.writeHead(200, {'Content-Type': 'application/json'});
         if (url === '/json') {
-            res.end(JSON.stringify(product));
+            mongoClient.connect(function(err, client){
+                if(err){
+                    return console.log(err);
+                }
+                const db = client.db("hw7");
+                const collection = db.collection("cities");
+                const city = {
+                    name: 'Brest',
+                    country: 'Belarus',
+                    capital: false,
+                    location: { lat: 52, long: 23 }
+                };
+                collection.insertOne(city, function(err, result){
+                    if(err){
+                        return console.log(err);
+                    }
+                    console.log(result.ops);
+                });
+                collection.find().toArray(function(err, results){
+                    client.close();
+                    res.end(JSON.stringify(results));
+                });
+            })
         }
-
     })
     .on('error', (error) => {
         throw new Error(error);
